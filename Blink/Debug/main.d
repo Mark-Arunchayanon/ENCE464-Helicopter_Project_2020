6ae65e0308e18e7957494932b940732fb71bcd46
Mark Arunchayanon <par116@canterbury.ac.nz>
2020-08-07 15:43:58 +1200

    trying to implement ADC queue, breaks when spun too fast

---
+++ Blink/Debug/main.d
@@ -0,0 +1,103 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h
+main.obj: ../FreeRTOSConfig.h
+main.obj: ../FreeRTOS.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/projdefs.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/portable.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/deprecated_definitions.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/portable/CCS/ARM_CM4F/portmacro.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/mpu_wrappers.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/task.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/list.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/semphr.h
+main.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/queue.h
+main.obj: ../altitude.h
+main.obj: ../circBufT.h
+main.obj: ../uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/time.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/abi_prefix.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/xlocale/_time.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h
+main.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h
+main.obj: ../OrbitOLED/OrbitOLEDInterface.h
+main.obj: ../yaw.h
+main.obj: ../buttons4.h
+
+../main.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h: 
+../FreeRTOSConfig.h: 
+../FreeRTOS.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/projdefs.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/portable.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/deprecated_definitions.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/portable/CCS/ARM_CM4F/portmacro.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/mpu_wrappers.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/task.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/list.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/semphr.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/queue.h: 
+../altitude.h: 
+../circBufT.h: 
+../uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/time.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/abi_prefix.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/xlocale/_time.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h: 
+../OrbitOLED/OrbitOLEDInterface.h: 
+../yaw.h: 
+../buttons4.h: 
---
+++ Blink/Debug/yaw.d
@@ -0,0 +1,89 @@
+# FIXED
+
+yaw.obj: ../yaw.c
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+yaw.obj: ../FreeRTOS.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h
+yaw.obj: ../FreeRTOSConfig.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/projdefs.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/portable.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/deprecated_definitions.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/portable/CCS/ARM_CM4F/portmacro.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/mpu_wrappers.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/task.h
+yaw.obj: P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/list.h
+yaw.obj: ../uart.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+yaw.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/time.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/abi_prefix.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/xlocale/_time.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h
+yaw.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h
+yaw.obj: ../OrbitOLED/OrbitOLEDInterface.h
+
+../yaw.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+../FreeRTOS.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h: 
+../FreeRTOSConfig.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/projdefs.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/portable.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/deprecated_definitions.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/portable/CCS/ARM_CM4F/portmacro.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/mpu_wrappers.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/task.h: 
+P:/Courses/ENCE464/Other//\ files/Blinker/Blink/FreeRTOS/include/list.h: 
+../uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/time.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/abi_prefix.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/xlocale/_time.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h: 
+../OrbitOLED/OrbitOLEDInterface.h: 
