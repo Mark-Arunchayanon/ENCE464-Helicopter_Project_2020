965da4cf45a2c9a32088b54431b5a7304ad5ee7b
Mark Arunchayanon <par116@canterbury.ac.nz>
2020-08-10 12:00:36 +1200

    Added an up-to-date project, will be working from this version

---
+++ Helicopter Project/Blink/display.c
@@ -0,0 +1,145 @@
+//*****************************************************************************
+//
+//  Display - Functions used to output values on Display and UART
+//
+//  Author:  N. James
+//           L. Trenberth
+//           M. Arunchayanon
+//     Last modified:   20.4.2019
+//*****************************************************************************
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
+#include "driverlib/debug.h"
+#include "utils/ustdlib.h"
+#include "stdlib.h"
+
+#include "display.h"
+//#include "system.h"
+#include "OrbitOLED/OrbitOLEDInterface.h"
+#include "OrbitOLED/lib_OrbitOled/OrbitOled.h"
+#include "altitude.h"
+#include "yaw.h"
+//#include "control.h"
+#include "uart.h"
+#include "buttons4.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+
+
+extern int32_t percentAlt;
+extern int32_t degrees;
+
+//extern int32_t AltRef;
+//extern int32_t YawRef;
+//  *****************************************************************************
+//  initDisplay:        Initialises Display using OrbitLED functions
+void initDisplay (void)
+{
+    // Initialise the Orbit OLED display
+    OLEDInitialise ();
+}
+
+
+//  *****************************************************************************
+//  introLine:          Prints the intro line on the OLED Display
+void introLine (void)
+{
+    OLEDStringDraw ("Heli Project", 0, 0);
+}
+
+
+//  *****************************************************************************
+//  printString:        Prints the input format and line contents on the given line number on OLED Display
+//  TAKES:              line_format - The format to print the string in, including a integer placeholder
+//                      line_contents - The integer to print on the line
+//                      line_number - The line number integer to print the string on.
+void printString(char* restrict line_format, int32_t line_contents, uint8_t line_number)
+{
+    char string[MAX_STR_LEN + 1];
+    usnprintf (string, sizeof(string), line_format, line_contents);
+    OLEDStringDraw (string, 0, line_number); // Update line on display.
+}
+
+
+//*****************************************************************************
+//  initButtonCheck:    Initialises left and up buttons on the micro-controller
+void initButtonCheck (void) {
+    SysCtlPeripheralReset (LEFT_BUT_PERIPH);//setting up the LEFT button GPIO
+    SysCtlPeripheralReset (UP_BUT_PERIPH);//setting the UP button GPIO
+    SysCtlPeripheralReset (DOWN_BUT_PERIPH);//setting the DOWN button GPIO
+    SysCtlPeripheralReset (RIGHT_BUT_PERIPH);//setting the RIGHT button GPIO
+}
+
+
+////  *****************************************************************************
+////  OutputToDisplay:    Displays the helicopter altitude, height and references
+////  NOTE:               This function is not currently implemented, though is included for testing
+//void OutputToDisplay (void)
+//{
+//    printString("Altitude = %4d%%", percentAlt, 0);
+////    printString("Yaw Angle = %4d", getYaw(), 1);
+////    printString("Alt Ref = %4d", GetAltRef(), 2);
+////    printString("Yaw Ref = %4d", GetYawRef(), 3);
+//}
+
+
+////*****************************************************************************
+////  OutputToDisplay:    Uses UART to send yaw and altitude references,
+////                      duty cycles and the current mode
+//void OutputToUART (void)
+//{
+//    char statusStr[MAX_STR_LEN + 1];
+//    if (slowTick)
+//    {
+//        slowTick = false;
+////        usprintf (statusStr, "YawRef=%2d Yaw=%2d | \r\n", GetYawRef(), getYaw()); // Form status message
+////        UARTSend (statusStr); // Send to the console
+//        usprintf (statusStr, "AltRef=%2d Alt=%2d | \r\n", percentAlt, percentAlt);
+//        UARTSend (statusStr);
+////        usprintf (statusStr, "MDut=%2d TDuty=%2d | \r\n", getMainDuty(), getTailDuty());
+////        UARTSend (statusStr);
+////        usprintf (statusStr, "Mode=%s | \r\n", getMode());
+////        UARTSend (statusStr);
+//    }
+//}
+
+void vDisplayTask (void *pvParameters)
+{
+    char statusStr[MAX_STR_LEN + 1];
+    const TickType_t xDelay1s = pdMS_TO_TICKS(100);
+
+    for ( ;; )
+    {
+        printString("Altitude = %4d%%", percentAlt, 0);
+        printString("Yaw = %4d%%", degrees, 1);
+
+//        usprintf (statusStr, "\033[2J\033[H Alt = %2d | Yaw = %2d |\n\r"
+//                "AltRef = %2d | YawRef = %2d |", percentAlt, degrees, AltRef, YawRef);
+//        UARTSend (statusStr);
+        usprintf (statusStr, "\033[2J\033[H Alt = %2d | Yaw = %2d |\n\r", percentAlt, degrees);
+        UARTSend (statusStr);
+
+//        usprintf (statusStr, "AltRef = %2d | YawRef = %2d |", AltRef, YawRef);
+//        UARTSend (statusStr);
+//        usprintf (statusStr, "AltRef=%2d Alt=%2d | YawRef=%2d Yaw=%2d | \033[2J\033[H", percentAlt, percentAlt);
+//        UARTSend (statusStr);
+//        usprintf (statusStr, "YawRef=%2d Yaw=%2d | \033[2J\033[H", degrees, degrees);
+//        UARTSend (statusStr);
+
+        vTaskDelay(xDelay1s);
+    }
+}
