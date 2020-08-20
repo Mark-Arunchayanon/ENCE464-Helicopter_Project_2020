2e940f8d5fd22db6bf0b82b944fad44d6993450b
George Thiele <get39@canterbury.ac.nz>
2020-08-21 11:49:06 +1200

    Modified comments and added title comment to buttons4.c

--- Helicopter Project/Blink/buttons4.c
+++ Helicopter Project/Blink/buttons4.c
@@ -1,17 +1,23 @@
-// *******************************************************
-// 
-// buttons4:        Support for a set of FOUR specific buttons on the Tiva/Orbit.
-//                  ENCE361 sample code.
-//                  The buttons are:  UP and DOWN (on the Orbit daughterboard) plus
-//                  LEFT and RIGHT on the Tiva.
-//
-//Note:             pin PF0 (the pin for the RIGHT pushbutton - SW2 on
-//                  the Tiva board) needs special treatment - See PhilsNotesOnTiva.rtf.
-//
-// P.J. Bones UCECE
-// Last modified:  7.2.2018
-// 
-// *******************************************************
+/***************************************************************************************
+ *
+ * ENCE464 FreeRTOS Helicopter Rig Controller Project
+ *
+ * buttons4:        Support for a set of FOUR specific buttons on the Tiva/Orbit.
+ *                  ENCE361 sample code.
+ *                  The buttons are:  UP and DOWN (on the Orbit daughterboard) plus
+ *                  LEFT and RIGHT on the Tiva.
+ *
+ * Note:             pin PF0 (the pin for the RIGHT pushbutton - SW2 on
+ *                   the Tiva board) needs special treatment - See PhilsNotesOnTiva.rtf.
+ *
+ * Original Author:        P.J. Bones UCECE
+ * Updated to FreeRTOS by: G. Thiele
+ *                         M. Arunchayanon
+ *                         S. Goonatillake
+ * Last modified:  21.08.2020
+ *
+ ***************************************************************************************
+ */
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -30,7 +36,7 @@
 
 // *******************************************************
 // Globals to module
-static bool but_state[NUM_BUTS];	// Corresponds to the electrical state
+static bool but_state[NUM_BUTS];    // Corresponds to the electrical state
 static uint8_t but_count[NUM_BUTS];
 static bool but_flag[NUM_BUTS];
 static bool but_normal[NUM_BUTS];   // Corresponds to the electrical state
@@ -41,16 +47,16 @@ static bool but_normal[NUM_BUTS];   // Corresponds to the electrical state
 void
 initButtons (void)
 {
-	int i;
+    int i;
 
-	// UP button (active HIGH)
+    // UP button (active HIGH)
     SysCtlPeripheralEnable (UP_BUT_PERIPH);
     while(!SysCtlPeripheralReady(UP_BUT_PERIPH));
     GPIOPinTypeGPIOInput (UP_BUT_PORT_BASE, UP_BUT_PIN);
     GPIOPadConfigSet (UP_BUT_PORT_BASE, UP_BUT_PIN, GPIO_STRENGTH_2MA,
        GPIO_PIN_TYPE_STD_WPD);
     but_normal[UP] = UP_BUT_NORMAL;
-	// DOWN button (active HIGH)
+    // DOWN button (active HIGH)
     SysCtlPeripheralEnable (DOWN_BUT_PERIPH);
     while(!SysCtlPeripheralReady(DOWN_BUT_PERIPH));
     GPIOPinTypeGPIOInput (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN);
@@ -79,12 +85,12 @@ initButtons (void)
        GPIO_PIN_TYPE_STD_WPU);
     but_normal[RIGHT] = RIGHT_BUT_NORMAL;
 
-	for (i = 0; i < NUM_BUTS; i++)
-	{
-		but_state[i] = but_normal[i];
-		but_count[i] = 0;
-		but_flag[i] = false;
-	}
+    for (i = 0; i < NUM_BUTS; i++)
+    {
+        but_state[i] = but_normal[i];
+        but_count[i] = 0;
+        but_flag[i] = false;
+    }
 }
 
 // *******************************************************
@@ -99,30 +105,30 @@ initButtons (void)
 void
 updateButtons (void)
 {
-	bool but_value[NUM_BUTS];
-	int i;
-	
-	// Read the pins; true means HIGH, false means LOW
-	but_value[UP] = (GPIOPinRead (UP_BUT_PORT_BASE, UP_BUT_PIN) == UP_BUT_PIN);
-	but_value[DOWN] = (GPIOPinRead (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN) == DOWN_BUT_PIN);
+    bool but_value[NUM_BUTS];
+    int i;
+
+    // Read the pins; true means HIGH, false means LOW
+    but_value[UP] = (GPIOPinRead (UP_BUT_PORT_BASE, UP_BUT_PIN) == UP_BUT_PIN);
+    but_value[DOWN] = (GPIOPinRead (DOWN_BUT_PORT_BASE, DOWN_BUT_PIN) == DOWN_BUT_PIN);
     but_value[LEFT] = (GPIOPinRead (LEFT_BUT_PORT_BASE, LEFT_BUT_PIN) == LEFT_BUT_PIN);
     but_value[RIGHT] = (GPIOPinRead (RIGHT_BUT_PORT_BASE, RIGHT_BUT_PIN) == RIGHT_BUT_PIN);
-	// Iterate through the buttons, updating button variables as required
-	for (i = 0; i < NUM_BUTS; i++)
-	{
+    // Iterate through the buttons, updating button variables as required
+    for (i = 0; i < NUM_BUTS; i++)
+    {
         if (but_value[i] != but_state[i])
         {
-        	but_count[i]++;
-        	if (but_count[i] >= NUM_BUT_POLLS)
-        	{
-        		but_state[i] = but_value[i];
-        		but_flag[i] = true;	   // Reset by call to checkButton()
-        		but_count[i] = 0;
-        	}
+            but_count[i]++;
+            if (but_count[i] >= NUM_BUT_POLLS)
+            {
+                but_state[i] = but_value[i];
+                but_flag[i] = true;    // Reset by call to checkButton()
+                but_count[i] = 0;
+            }
         }
         else
-        	but_count[i] = 0;
-	}
+            but_count[i] = 0;
+    }
 }
 
 // *******************************************************
@@ -132,29 +138,28 @@ updateButtons (void)
 uint8_t
 checkButton (uint8_t butName)
 {
-	if (but_flag[butName])
-	{
-		but_flag[butName] = false;
-		if (but_state[butName] == but_normal[butName])
-			return RELEASED;
-		else
-			return PUSHED;
-	}
-	return NO_CHANGE;
+    if (but_flag[butName])
+    {
+        but_flag[butName] = false;
+        if (but_state[butName] == but_normal[butName])
+            return RELEASED;
+        else
+            return PUSHED;
+    }
+    return NO_CHANGE;
 }
 
-
+// *******************************************************
+// vButtonTask:     Function returns the new button logical state if the button
+//                  logical state (PUSHED or RELEASED) has changed since the last call,
+//                  otherwise returns NO_CHANGE.
 void vButtonTask (void *pvParameters)
 {
     const TickType_t xDelay1s = pdMS_TO_TICKS(10);
-//    char statusStr[MAX_STR_LEN + 1];
 
     for ( ;; )
     {
         updateButtons();
-
-//        usprintf (statusStr, "\r\nButton working");
-//        UARTSend (statusStr);
 
         vTaskDelay(xDelay1s);
     }
