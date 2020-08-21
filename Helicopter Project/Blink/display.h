99139ce8f89a89bf7c0bdce6001e0fe191b5d9e7
George Thiele <get39@canterbury.ac.nz>
2020-08-21 14:56:36 +1200

    Updated display.c and display.h comments

--- Helicopter Project/Blink/display.h
+++ Helicopter Project/Blink/display.h
@@ -1,18 +1,22 @@
 #ifndef DISPLAY_H_
 #define DISPLAY_H_
+/*************************************************************************************************
+ *
+ * ENCE464 FreeRTOS Helicopter Rig Controller Project
+ *
+ * display:         Functions used to output values on Display and UART
+ *
+ * Original Authors:       N. James
+ *                         L. Trenberth
+ *                         M. Arunchayanon
+ * Updated to FreeRTOS by: G. Thiele
+ *                         M. Arunchayanon
+ *                         S. Goonatillake
+ * Last modified:  21.08.2020
+ *
+ ************************************************************************************************/
 
-//*****************************************************************************
-//
-// Display - Display related functions
-//
-// Author:  N. James
-//          L. Trenberth
-//          M. Arunchayanon
-// Last modified:   20.4.2019
-//*****************************************************************************
 
-
-//#include "system.h"
 #include "OrbitOLED/OrbitOLEDInterface.h"
 #include "OrbitOLED/lib_OrbitOled/OrbitOled.h"
 #include "altitude.h"
@@ -20,48 +24,43 @@
 #include "semphr.h"
 
 
-extern SemaphoreHandle_t xDisplayMutex;
-
-//  *****************************************************************************
-//  initDisplay:        Initialises Display using OrbitLED functions
-void
-initDisplay (void);
+/***********************************************************************************************
+ * Constants/Definitions
+ **********************************************************************************************/
+extern SemaphoreHandle_t xDisplayMutex; // Create mutex for controlling access to yaw and altitude.
 
 
-//  *****************************************************************************
-//  introLine:          Prints the intro line on the OLED Display
-void
-introLine (void);
+/***********************************************************************************************
+ * initDisplay:        Initialises Display using OrbitLED functions
+ **********************************************************************************************/
+void initDisplay (void);
 
 
-//  *****************************************************************************
-//  printString:        Prints the input format and line contents on the given line number on OLED Display
-//  TAKES:              line_format - The format to print the string in, including a integer placeholder
-//                      line_contents - The integer to print on the line
-//                      line_number - The line number integer to print the string on.
-void
-printString(char* restrict line_format, int32_t line_contents, uint8_t line_number);
+/***********************************************************************************************
+ * introLine:          Prints the intro line on the OLED Display
+ **********************************************************************************************/
+void introLine (void);
 
 
-//*****************************************************************************
-//  initButtonCheck:    Initialises left and up buttons on the micro-controller
-void
-initButtonCheck (void);
+/***********************************************************************************************
+ * printString:        Prints the input format and line contents on the given line number on OLED Display
+ * TAKES:              line_format - The format to print the string in, including a integer placeholder
+ *                     line_contents - The integer to print on the line
+ *                     line_number - The line number integer to print the string on.
+ **********************************************************************************************/
+void printString(char* restrict line_format, int32_t line_contents, uint8_t line_number);
 
 
-//  *****************************************************************************
-//  OutputToDisplay:    Displays the helicopter altitude, height and references
-//  NOTE:               This function is not currently implemented, though is included for testing
-//void
-//OutputToDisplay (void);
+/***********************************************************************************************
+ * initButtonCheck:    Initialises left and up buttons on the micro-controller
+ **********************************************************************************************/
+void initButtonCheck (void);
 
 
-//*****************************************************************************
-//  OutputToDisplay:    Uses UART to send yaw and altitude references,
-//                      duty cycles and the current mode
-//void
-//OutputToUART (void);
-
+/***********************************************************************************************
+ * vDisplayTask:       A task scheduled by FreeRTOS to update the display. Prints a readout of
+ *                     position and control details to the display and UART. Has a task priority of 3.
+ **********************************************************************************************/
 void vDisplayTask (void *pvParameters);
 
 
