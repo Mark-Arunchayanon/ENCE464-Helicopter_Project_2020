7e5a92290e8911a00512d7ff3b51630ad2ec7374
George Thiele <get39@canterbury.ac.nz>
2020-08-21 16:22:55 +1200

    Moved button constants back to .h file to stop compiling errors.

--- Helicopter Project/Blink/buttons4.c
+++ Helicopter Project/Blink/buttons4.c
@@ -39,34 +39,6 @@
 #include "uart.h"
 
 
-/***********************************************************************************************
- * Constants/Definitions
- **********************************************************************************************/
-enum butNames {UP = 0, DOWN, LEFT, RIGHT, NUM_BUTS};
-enum butStates {RELEASED = 0, PUSHED, NO_CHANGE};
-
-// UP button
-#define UP_BUT_PERIPH  SYSCTL_PERIPH_GPIOE
-#define UP_BUT_PORT_BASE  GPIO_PORTE_BASE
-#define UP_BUT_PIN  GPIO_PIN_0
-#define UP_BUT_NORMAL  false
-// DOWN button
-#define DOWN_BUT_PERIPH  SYSCTL_PERIPH_GPIOD
-#define DOWN_BUT_PORT_BASE  GPIO_PORTD_BASE
-#define DOWN_BUT_PIN  GPIO_PIN_2
-#define DOWN_BUT_NORMAL  false
-// LEFT button
-#define LEFT_BUT_PERIPH  SYSCTL_PERIPH_GPIOF
-#define LEFT_BUT_PORT_BASE  GPIO_PORTF_BASE
-#define LEFT_BUT_PIN  GPIO_PIN_4
-#define LEFT_BUT_NORMAL  true
-// RIGHT button
-#define RIGHT_BUT_PERIPH  SYSCTL_PERIPH_GPIOF
-#define RIGHT_BUT_PORT_BASE  GPIO_PORTF_BASE
-#define RIGHT_BUT_PIN  GPIO_PIN_0
-#define RIGHT_BUT_NORMAL  true
-
-#define NUM_BUT_POLLS 3
 // Debounce algorithm:  A state machine is associated with each button.
 //                      A state change occurs only after NUM_BUT_POLLS consecutive polls have
 //                      read the pin in the opposite condition, before the state changes and
