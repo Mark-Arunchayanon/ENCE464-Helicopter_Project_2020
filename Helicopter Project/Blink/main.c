9221c858879023129fefff5b49550802ff1b2bf3
George Thiele <get39@canterbury.ac.nz>
2020-08-21 13:59:40 +1200

    main.c commenting complete.

--- Helicopter Project/Blink/main.c
+++ Helicopter Project/Blink/main.c
@@ -1,10 +1,10 @@
-/**
- * Simple LED blinking example for the Tiva Launchpad
- */
-
-/***************************************************************************************
+/*************************************************************************************************
  *
  * ENCE464 FreeRTOS Helicopter Rig Controller Project
+ *
+ * The main file for the UC ENCE464 2020 HeliRig assignment.
+ * Controls a helicopter using a FreeRTOS scheduler on Tiva CPU. The helicopter can achieve
+ * stable flight using a PID controller, and is controlled using the buttons on the Tiva board.
  *
  * main:            Initialises and resets peripherals, creates FreeRTOS tasks for
  *                  controlling the helicopter, and runs the program using a task
@@ -15,8 +15,7 @@
  *                     S. Goonatillake
  * Last modified:  21.08.2020
  *
- ***************************************************************************************
- */
+ ************************************************************************************************/
 
 
 /*************************************************************************************************
@@ -51,17 +50,27 @@
 #include "control.h"
 #include "display.h"
 
+
+/***********************************************************************************************
+ * Constants/Definitions
+ **********************************************************************************************/
 #define BUF_SIZE                    10
 #define TASK_STACK_DEPTH            128
 #define DISPLAY_TASK_STACK_DEPTH    768
 
+// Task priority levels, higher numbers will be executed first.
 #define BUTTON_TASK_PRIORITY        3
 #define CONTROL_TASK_PRIORITY       4
 #define ADCSAMPLE_TASK_PRIORITY     2
 #define ADC_TASK_PRIORITY           2
 #define DISPLAY_TASK_PRIORITY       3
 
+
+/***********************************************************************************************
+ * Global Variables
+ **********************************************************************************************/
 static TaskHandle_t xPIDTask = NULL;
+
 
 int main(void)
 {
@@ -80,29 +89,33 @@ int main(void)
     initSwitch_PC4();
     IntMasterEnable();
 
+    // Create buttons Task
     if (pdTRUE != xTaskCreate(vButtonTask, "Buttons", TASK_STACK_DEPTH, NULL, BUTTON_TASK_PRIORITY, NULL))
-    { // (void *)1 is our pvParameters for our task func specifying PF_1
+    {
         while (1); // Error creating task
     }
 
+    // Create control Task
     if (pdTRUE != xTaskCreate(vControlTask, "Control", TASK_STACK_DEPTH, NULL, CONTROL_TASK_PRIORITY, &xPIDTask))
-    { // (void *)1 is our pvParameters for our task func specifying PF_1
+    {
+        while (1); // Error creating task
+    }
+
+    // Create ADC Sampler Task
+    if (pdTRUE != xTaskCreate(vADCSampleTask, "ADC Sampler", TASK_STACK_DEPTH, NULL, ADCSAMPLE_TASK_PRIORITY, NULL))
+    {
         while (1); // Error creating task
     }
 
     // Create ADC Task
-    if (pdTRUE != xTaskCreate(vADCSampleTask, "ADC Sampler", TASK_STACK_DEPTH, NULL, ADCSAMPLE_TASK_PRIORITY, NULL))
-    { // (void *)1 is our pvParameters for our task func specifying PF_1
-        while (1); // Error creating task
-    }
-
     if (pdTRUE != xTaskCreate(vADCTask, "ADC Calc", TASK_STACK_DEPTH, xPIDTask, ADC_TASK_PRIORITY, NULL))
-    { // (void *)1 is our pvParameters for our task func specifying PF_1
+    {
         while (1); // Error creating task
     }
 
+    // Create Display Task
     if (pdTRUE != xTaskCreate(vDisplayTask, "Display", DISPLAY_TASK_STACK_DEPTH, NULL, DISPLAY_TASK_PRIORITY, NULL))
-    { // (void *)1 is our pvParameters for our task func specifying PF_1
+    {
         while (1); // Error creating task
     }
 
